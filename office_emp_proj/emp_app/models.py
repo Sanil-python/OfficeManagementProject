39b7a116ea639f69d989dd4c454bd6e29463d68c
Sanil Kumar Barik <sanilbarik18@gmail.com>
2022-09-25 21:06:32 +0530

    Second Commit

--- office_emp_proj/emp_app/models.py
+++ office_emp_proj/emp_app/models.py
@@ -24,6 +24,6 @@ class Employee(models.Model):
     role = models.ForeignKey(Role,on_delete=models.CASCADE)
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
-    
+        
     def __str__(self):
         return "%s %s %s" %(self.first_name,self.last_name,self.phone)
